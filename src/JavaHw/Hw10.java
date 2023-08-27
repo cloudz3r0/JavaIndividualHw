6e27e2ec7427d689193a04c8ad14b741fe5a96c9
cloudz3r0 <ren7ero@gmail.com>
2023-08-27 02:31:15 -0400

    second commit

--- src/JavaHw/Hw10.java
+++ src/JavaHw/Hw10.java
@@ -5,7 +5,14 @@ public class Hw10 {
         /*
          Write a program to print out duplicate elements from an Array of
 Strings?
-
-         */
+*/
+    String []arr={"Hello","Hi","My","Hello","Hi","Help"};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j <arr.length; j++) {
+                if(arr[i].equals(arr[j])){
+                    System.out.println(arr[i]);
+                }
+            }
+        }
     }
 }
--- src/JavaHw/Hw9.java
+++ src/JavaHw/Hw9.java
@@ -6,5 +6,19 @@ public class Hw9 {
        Write a java program to find the second
        largest number in the array?
        */
-    }
-}
+
+        int[] numbers = {5, 10, 20, 8, 15, 7, 3};
+    int secondLargest=0;
+    int largest=0;
+
+        for (int num : numbers) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest) {
+                secondLargest = num;
+            }
+        }
+        System.out.println(secondLargest);
+            }
+        }
